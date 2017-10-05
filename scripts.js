2bac5e4adf3386a44e019e33f4032a38c681f252
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-10-05 18:23:48 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/*Variables*/
+var friendNames = ["Brandon", "Erica", "Tyffani" , "Daniel", "Kirsten"]
+
+var weapons = ["Tiger Woods golf club", "Malaria", "Rihanna\'s thighs", "Exploding Kitten", "Godzilla",
+                "Explosive Diarrhea indeuced from Taco Bell consumption", "live action Dragon Ball movie", "The Fast and Furious franchise",
+                "a Beats Pill", "a baby penguin", "a spoon", "a stone cold stunner", "Dane Cook comedy show",
+                "ASMR", "a thick Scottish accent", "Hallmark Channel marathon", "literally any javascript lab",
+                "sushi poisoning", "the stingray that killed Steve Irwin", "Timone from Lion King" ]
+
+ var scenes = ["that one local pizza shopped owned by that one Italian family that knows my Italian family that talks about Italian stuff really loudly",
+            "the local Applebees", "dimly lit ballroom", "Tron world", "Jurassic Park", "Atlantis", "the VMAs", "the dropoff point of Jim Carreys career",
+            "the sandbox", "the nude beach"]
+
+
+function randomizedWeapon(){
+    console.log(weapons);
+}
+
+console.log(randomizedWeapon);
